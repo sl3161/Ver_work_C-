b3ae4e324ba96c81751faa29ea72c0da98f3970e
«Zotov <sl3161@yandex.ru>
2023-12-31 17:18:14 +0300

    методы получения нового массива

--- Ver_work.cs
+++ Ver_work.cs
@@ -6,7 +6,36 @@ string [] input_arr = new string[4]{"Hello","3r", "H-", "(-" };
 System.Console.WriteLine($" массив :   ");
 PrintArray(input_arr);
 System.Console.WriteLine($" Новый массив :  ");
-//PrintArray(GetNewArr(input_arr));
+PrintArray(GetNewArr(input_arr));
+
+string [] GetNewArr (string [] input_array)       // Формирует новый массив
+{
+    int len = GetLengthNewArr(input_arr);
+    string [] res = new string [len];
+    int j=0;
+    for (int i = 0; i < input_arr.Length-1; i++)
+    {
+        if (input_arr[i].Length<=3)
+        {
+         res[j]=input_arr[i];
+         j++;
+        }
+    }
+    return res;   
+}
+
+
+int GetLengthNewArr (string [] input_arr)         // Возвращает кол во элементов удовлетворяющих условию
+{
+    int count = 0;
+    string [] res = new string [input_arr.Length];
+    for (int i = 0; i < input_arr.Length-1; i++)
+    {
+        if (input_arr[i].Length<=3) count ++;
+    }
+    return count;
+
+}
 
 
 
